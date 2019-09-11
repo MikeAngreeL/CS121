176137df37f0b0e67e1eca83bcb9412bc1e477b6
Michael Long <46852314+MikeAngreeL@users.noreply.github.com>
2019-09-11 11:20:12 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,44 +1,46 @@
-//CS121-03 Assignment #1 
+// CS121-03 Assignment #1 
 // Michael Long
-//This assignment reads from an external file and translates salaries for hours worked for employees. given that they are in the same 4 line format throughout
+// This assignment reads from an external file and translates salaries for hours worked for employees. given that they are in the same 4 line format throughout
 
-#include <iostream>
-#include <string> // allows string operations
-#include <fstream> // allows for reading and writing from a file.
-#include <iomanip> // allows for formatting of numbers
+#include <iostream>     // needed for main operations
+#include <string>       // allows string operations
+#include <fstream>      // allows for reading and writing from a file.
+#include <iomanip>      // allows for formatting of numbers
 
 using namespace std;
 
 int main()
-{ // beginning of main
+{       // beginning of main and declaration of variables 
     ifstream myfile;
     string employeeId;
     string employeeName;
     string hours;
     string pay;
     float totalpay;
-    cout << fixed;
-    cout << setprecision(2);
+    
+    cout << fixed;           // sets the base for the numbers to allow precision formatting
+    cout << setprecision(2); // sets the decimal count to 2 for formatting
 
-    myfile.open("hours.txt"); // opens hours.txt to allow use of the file
+    myfile.open("hours.txt");       // opens hours.txt to allow use of the file
 
-    cout << "\t\t[EMPLOYEE PAYROLL FOR THE WEEK]" << endl;//top of header
-    cout << "******************************************************************************" << endl;//serperator line
-    while(!myfile.eof())
-    {//the beginning of while loop
+    cout << "\t\t[EMPLOYEE PAYROLL FOR THE WEEK]" << endl;      //top of header
+    cout << "*************************************************************************" << endl;       //serperator line
+    
+    while(!myfile.eof())    //loops as long as we are not at the end of the file
+    {// the beginning of while loop to read through the file
         getline(myfile,employeeId);
-        //cout << employeeId << endl; tested to see if it printed correctly
+        //cout << employeeId << endl;                   tested to see if it printed correctly
         
         getline(myfile, employeeName);
-        //cout << employeeName << endl; tested to see if printed correctly
+        //cout << employeeName << endl;                 tested to see if printed correctly
         
         getline(myfile, hours);
-        float hoursWorked = stof(hours);//conversion of string to float
+        float hoursWorked = stof(hours);                 // conversion of string to float
         
         getline(myfile, pay);
-        float payForWork = stof(pay);//conversion of string to float
+        float payForWork = stof(pay);                    // conversion of string to float
         
-        totalpay = hoursWorked * payForWork;//math to calculate total pay for the week
+        totalpay = hoursWorked * payForWork;            // math to calculate total pay for the week
         //cout << totalpay; tested if pay calculation worked
 
         //cout << "Employee ID: " << employeeId << '\t' << "Employee Name: " << employeeName << '\t' << "Total pay: " << totalpay << endl;
@@ -48,10 +50,13 @@ int main()
             cout << "Employee ID: " << employeeId << '\t' << "Employee Name: " << employeeName << '\t' << "Total pay: " << totalpay << endl;
         }
     }//end of while loop
-    cout << "******************************************************************************" << endl;//bottom seperator line
+   
+    cout << "*************************************************************************" << endl;       //bottom seperator line
 
-    myfile.close();//closes the txt file.
-    //system("pause");
+    myfile.close();     //closes the txt file.
+    
+    //system("pause"); //{FIXME FOR WINDOWS MACHINES}
+
 
     return 0;
 }//end of main
